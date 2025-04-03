f9f581041272f6eabc95c2b7b47dbc87f01f93bf
kettlycl <kettlycampos2gmail.com>
2025-04-03 16:28:44 -0400

    variáveis e tipos de Dados

---
+++ aula-01.js
@@ -0,0 +1,7 @@
+let valor = 13.141416;
+let text = 'meu primeiro projeto em JavaScript';
+let bool = true;
+
+console.log(valor.toFixed(2)); //Arredonda o valor para duas casas decimais
+console.log(text.toLocaleUpperCase());//Converte o texto para maiúsculas
+console.log(ativo.toString());//converte para texto
